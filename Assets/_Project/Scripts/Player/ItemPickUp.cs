0e1809e957e6dfac6596ebf903901aa525edc0ae
Aaron Dahlhauser <aarndal@duck.com>
2024-05-22 16:00:52 +0200

    Fixed ItemPickUp

--- Assets/_Project/Scripts/Player/ItemPickUp.cs
+++ Assets/_Project/Scripts/Player/ItemPickUp.cs
@@ -8,6 +8,7 @@ public class ItemPickUp : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Piep");
         if (other.TryGetComponent(out ICollectable collectable)) collectable.Collect(other);
     }
 }
